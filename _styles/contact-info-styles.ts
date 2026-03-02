e0dbb179c9ab6b9454392166796494829504d18e
Chad Wright <justdeejays@gmail.com>
2026-03-02 11:42:52 +0200

    fix: update contact info styles

--- _styles/contact-info-styles.ts
+++ _styles/contact-info-styles.ts
@@ -2,7 +2,7 @@ import classNames from "classnames";
 
 export const ContactInfoStyles = (cssClasses?: string, isButton?: boolean) => {
   const baseClasses =
-    "px-2 text-left -mx-2 text-paragraph font-medium py-3 -my-3 hover:tablet:opacity-80 desktop:p-0 desktop:m-0 hover:desktop-small:text-grey leading-[40px] italic";
+    "text-[16px] px-2 text-left -mx-2 font-medium py-3 -my-3 min-[450px]:text-paragraph hover:tablet:opacity-80 desktop:p-0 desktop:m-0 hover:desktop-small:text-grey leading-[40px] italic";
   const conditionalClasses = {
     "hover:cursor-pointer": isButton,
   };
